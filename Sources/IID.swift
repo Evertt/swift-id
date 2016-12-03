c18bb605b67c2be33b68934a5687d13dbe87ea11
Evert van Brussel <ecvanbrussel@gmail.com>
2016-12-03 09:07:34 +0700

    removed description from iid

--- Sources/IID.swift
+++ Sources/IID.swift
@@ -1,7 +1,6 @@
-public struct IID: ID, CustomStringConvertible {
+public struct IID: ID {
     public static var seed = 0
     public let hashValue: Int
-    public let description: String
 
     public init() {
         IID.seed += 1
@@ -10,34 +9,11 @@ public struct IID: ID, CustomStringConvertible {
 
     public init(_ id: Int) {
         hashValue = id
-        description = String(hashValue, radix: 16, uppercase: true)
-    }
-
-    public init?(_ string: String) {
-        guard let id = Int(string, radix: 16) else {
-            return nil
-        }
-
-        self = IID(id)
     }
 }
 
 extension IID: Comparable {    
     public static func <(left: IID, right: IID) -> Bool {
         return left.hashValue < right.hashValue
-    }
-}
-
-extension IID: ExpressibleByStringLiteral {
-    public init(stringLiteral value: String) {
-        self = IID(value)!
-    }
-    
-    public init(extendedGraphemeClusterLiteral value: String) {
-        self = IID(value)!
-    }
-    
-    public init(unicodeScalarLiteral value: String) {
-        self = IID(value)!
     }
 }
